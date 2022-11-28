a7aee4376a99cc65107aab80304459cb2e40dfd9
Pyry Pajunen <92794217+pyrypajunen@users.noreply.github.com>
2022-11-28 10:26:11 +0100

    Deploying RWD, Tether contracts, manual test and playing with truffle console

--- defi-yield-farming/truffle/migrations/2_deploy_contracts.js
+++ defi-yield-farming/truffle/migrations/2_deploy_contracts.js
@@ -3,13 +3,23 @@ const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = function (deployer) {
+module.exports = async function (deployer, network, accounts) {
    // deploy migrations
-   deployer.deploy(Migrations);
+   await deployer.deploy(Migrations);
    // deploy Tethet contract
-   deployer.deploy(Tether);
+   await deployer.deploy(Tether);
+   const tether = await Tether.deployed()
    // deploy RWD token
-   deployer.deploy(RWD);
+   await deployer.deploy(RWD);
+   const rwd = await RWD.deployed()
    // deploy DB contract
-   deployer.deploy(DecentralBank);
+   await deployer.deploy(DecentralBank, rwd.address, tether.address);
+   const db  = await DecentralBank.deployed()
+
+   // 1 million tokens transfer to DB
+   await rwd.transfer(db ,"1000000000000000000000000")
+
+   // Distribute 100 tether tokens to investor
+   await tether.transfer(accounts[1],"1000000000000000000")
+
 };
